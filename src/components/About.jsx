ff420c12846942d9f13ea09f79d8db7f0a662660
tung_nguyen12 <tony.sn@outlook.com>
2021-12-16 20:18:08 +1100

    Created About page to test React Route

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About</h1>
+
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut porro
+        accusamus tempora obcaecati perferendis tenetur quia aliquam facere
+        inventore fugiat at autem odit laboriosam minima ad itaque, quidem
+        dolorum ea maiores eos! Suscipit at iusto nisi necessitatibus. Adipisci
+        eos et minus ratione esse sed consequatur tenetur, necessitatibus
+        reiciendis nostrum. Soluta, iste! Saepe laborum expedita perferendis ea
+        eos itaque. Perspiciatis perferendis architecto quaerat tempora,
+        mollitia tempore asperiores molestiae minus quod maxime consequatur
+        aliquid quibusdam alias harum ad aperiam dolores. Sapiente architecto
+        consequuntur saepe iusto iure optio, eaque nobis quidem, mollitia
+        recusandae molestiae enim cumque velit repellendus aliquam? Quae
+        consequatur labore quaerat.
+      </div>
+    </div>
+  )
+}
+
+export default About
