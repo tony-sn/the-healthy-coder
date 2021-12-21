6b828f31967f18462f88854d304794c9f149fdb1
tung_nguyen12 <tony.sn@outlook.com>
2021-12-22 01:53:42 +1100

    Created ErrorPage

---
+++ src/pages/Error404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Error404() {
+  return (
+    <div className='container'>
+      <h1 className="title">404. PAGE NOT FOUND!</h1>
+
+    </div>
+  )
+}
+
+export default Error404
